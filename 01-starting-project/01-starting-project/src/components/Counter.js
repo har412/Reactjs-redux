1cde24f4750c884f9b72aa158bb130862fbb779c
Harkirat <hs3188248@gmail.com>
2023-02-02 12:34:39 +0530

    compeleted counter using function based components

--- 01-starting-project/01-starting-project/src/components/Counter.js
+++ 01-starting-project/01-starting-project/src/components/Counter.js
@@ -1,12 +1,32 @@
+// connect is used for class based component
+import { useDispatch, useSelector } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  // automatically adds the subscription so that the state is updated automatically
+  let counter = useSelector(state => state.counter);
+  const dispatch=useDispatch()
+  const toggleCounterHandler = () => {
+  dispatch({type:'toggle'})
+  };
+  
+  const increment = () => {
+    dispatch({type:'increment'})
+    // console.log("incre")
+    // counter = counter+1;
+  };
 
+  const decrement = () => {
+    dispatch({type:'decrement'})
+    // counter = counter-1;
+    //  console.log("incre")
+  };
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- 01-starting-project/01-starting-project/src/store/index.js
+++ 01-starting-project/01-starting-project/src/store/index.js
@@ -3,16 +3,24 @@ import { createStore } from 'redux';
 // reducer function
 const counterReducer = (state = {counter:0}, action) => {
 
-    if(action.type=='increment'){
+    if(action.type==='increment'){
     return {
         counter : state.counter + 1,
     }; 
 }
-if(action.type=='decrement'){
+
+if(action.type==='decrement'){
     return {
         counter : state.counter -1,
     };
-}
+}  
+if(action.type==='toggle'){
+    return {
+        counter : 0,
+    };
+}  
+
+return state;
 };
 
 const store = createStore(counterReducer);
