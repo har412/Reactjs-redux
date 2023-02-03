fa7f113699d00556005bc30268d06587e0288323
Harkirat <hs3188248@gmail.com>
2023-02-03 11:37:42 +0530

    used single object for all states in component

--- hooks/src/components/Ingredients/IngredientForm.js
+++ hooks/src/components/Ingredients/IngredientForm.js
@@ -5,7 +5,7 @@ import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
 
-  const inputState = useState({ title: '', amount: '' });
+  const [State,SetState] = useState({ title: '', amount: '' });
   const submitHandler = event => {
     event.preventDefault();
     // ...
@@ -17,11 +17,11 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" value={inputState[0].title} onChange={event =>{  const newTitle=event.target.value; inputState[1](prevInputState => ({ title: newTitle, amount: prevInputState.amount }))}} />
+            <input type="text" id="title" value={State.title} onChange={event =>{  const newTitle=event.target.value; SetState(prevInputState => ({ title: newTitle, amount: prevInputState.amount }))}} />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" value={inputState[0].amount} onChange={event => { const newAmount=event.target.value;inputState[1]({ amount:newAmount, title: inputState[0].title })}} />
+            <input type="number" id="amount" value={State.amount} onChange={event => { const newAmount=event.target.value;SetState({ amount:newAmount, title: State.title })}} />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
