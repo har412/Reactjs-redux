ab282f4349ba0401f6414dc3b5e1b9f59bcb3133
Harkirat <hs3188248@gmail.com>
2023-02-05 17:47:46 +0530

    use reducer implemented

--- hooks/src/components/Ingredients/Ingredients.js
+++ hooks/src/components/Ingredients/Ingredients.js
@@ -1,21 +1,37 @@
-import React, {useEffect, useState , useCallback} from 'react';
+import React, {useReducer ,useEffect, useState , useCallback} from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import Search from './Search';
 import ErrorModal from '../UI/ErrorModal'
+
+const ingredientReducer = (currentIngredients,action)=>{
+  switch(action.type){
+    case 'set':
+      return action.ingredient;
+    case 'add':
+      return [...currentIngredients,action.ingredient];
+    case 'delete':
+      return currentIngredients.filter(ing => ing.id !== action.id);
+    default: 
+      throw new Error('Should not get here')
+  }
+}
+
 function Ingredients() {
-  const [ingredients,setIngredients]=useState ([]);
+  const [ingredients,dispatch]= useReducer(ingredientReducer,[])
+  // const [ingredients,setIngredients]=useState ([]);
   const [isLoading,setLoading]= useState(false)
 const[Error,setError]=useState()
- 
+
   const filterIngredientHandler = useCallback( filterIngredients =>{
-    setIngredients(filterIngredients)
+    // setIngredients(filterIngredients)
+    dispatch({type:'set',ingredient:filterIngredients})
   },[])
   const addIngredientHandler = ingredient =>{
    console.log(ingredient)
    setLoading(true)
-    fetch('https://react-redux-311fedients.json',{
+    fetch('https://react-redux-311f1-default-rtdb.firebaseio.com/ingredients.json',{
       method:'POST',
       body: JSON.stringify(ingredient),
       headers:{'Content-type':'application/json'}
@@ -23,18 +39,20 @@ const[Error,setError]=useState()
       setLoading(false)
       return response.json()
     }).then(responseData => {
-      setIngredients(prevIngredients => [...prevIngredients,{id:responseData.name,...ingredient}])
+      // setIngredients(prevIngredients => [...prevIngredients,{id:responseData.name,...ingredient}])
+      dispatch({type:"add",ingredient:{id:responseData.name,...ingredient}})
     }).catch((err)=>{
       return setError('You have a error!')
     });
   }
   const removeIngredientHandler = ingredientId => {
     setLoading(true)
-    fetch(`https://react-redux-311f1-default-/ingredients/${ingredientId}.json`,{
+    fetch(`https://react-redux-311f1-default-rtdb.firebaseio.com/ingredients/${ingredientId}.json`,{
       method:'DELETE',
     }).then((response)=>{
       setLoading(false)
-      setIngredients(prevIngredients => prevIngredients.filter((ingredient)=>{ return ingredient.id !== ingredientId}))
+      // setIngredients(prevIngredients => prevIngredients.filter((ingredient)=>{ return ingredient.id !== ingredientId}))
+      dispatch({type:'delete',id:ingredientId})
     }).catch((err)=>{
       return setError('You have a error!')
     })
